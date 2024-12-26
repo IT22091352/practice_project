2bfb8310f04763a7393f1aebb90d0c4e37982ee1
Chathuka Dilakshana <IT22091352@my.sliit.lk>
2024-12-27 00:23:21 +0530

    fix: correct import paths for BusDetails and AddBus components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
-import BusDetails from "./Components/BusDetails/BusDetails"; 
-import AddBus from "./Components/AddBus/AddBus"; 
+import BusDetails from "./Components/BusDetails/busdetails";
+import AddBus from "./Components/AddBus/AddBus";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route path="/mainhome" component={Home} />
           <Route path="/addBus" component={AddBus} />
-          <Route path="/busdetails" component={BusDetails} /> 
+          <Route path="/busdetails" component={BusDetails} />
         </Switch>
       </React.Fragment>
     </div>
